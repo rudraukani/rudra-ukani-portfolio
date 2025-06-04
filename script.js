de4be0b8b410a288ce9ce6519b743bf5bba3122c
rudraukani <162187466+rudraukani@users.noreply.github.com>
2025-06-04 11:13:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.querySelector(".hamburger-icon");
-    const menu = document.querySelector(".menu-links");
-
-    hamburger.addEventListener("click", function() {
-        menu.classList.toggle("open");
-        hamburger.classList.toggle("open");
-    });
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
